56d6fb79252c352ffe6a7deee7f8c2cb0f97644a
cwilmott <76979489+cwilmott@users.noreply.github.com>
2025-10-25 17:03:31 -0700

    Update mapbox map configuration

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY3dpbG1vdHQiLCJhIjoiY2s2bWRjb2tiMG1xMjNqcDZkbGNjcjVraiJ9.2nNOYL23A1cfZSE4hdC9ew';
-    const map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: [-74.5, 40], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        style: 'mapbox://styles/cwilmott/cmg5px11u00ef01sm3fr65ro0',
+        center: [-122.27, 37.8], // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
